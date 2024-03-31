cc8d488c78dc6a7a0abdb2a2536da94535a4d71e
Ubuntu <ubuntu@ip-172-31-7-138.eu-west-1.compute.internal>
2024-03-31 23:55:35 +0000

    add dry mode

--- scripts/pushImages.sh
+++ scripts/pushImages.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-api-gateway:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-visits-service:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-vets-service:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-customers-service:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:latest
+
+# Si DRY_RUN est défini et égal à true, alors afficher les commandes sans les exécuter.
+if [ "${DRY_RUN}" = "true" ]; then
+  echo "Mode Dry Run activé. Les commandes suivantes seraient exécutées :"
+  echo "docker push ${REPOSITORY_PREFIX}/spring-petclinic-config-server:${VERSION}"
+  echo "docker push ${REPOSITORY_PREFIX}/spring-petclinic-discovery-server:${VERSION}"
+  echo "docker push ${REPOSITORY_PREFIX}/spring-petclinic-api-gateway:${VERSION}"
+  echo "docker push ${REPOSITORY_PREFIX}/spring-petclinic-visits-service:${VERSION}"
+  echo "docker push ${REPOSITORY_PREFIX}/spring-petclinic-vets-service:${VERSION}"
+  echo "docker push ${REPOSITORY_PREFIX}/spring-petclinic-customers-service:${VERSION}"
+  echo "docker push ${REPOSITORY_PREFIX}/spring-petclinic-admin-server:${VERSION}"
+else
+  # Mode normal : Exécuter les commandes de push.
+  docker push ${REPOSITORY_PREFIX}/spring-petclinic-config-server:${VERSION}
+  docker push ${REPOSITORY_PREFIX}/spring-petclinic-discovery-server:${VERSION}
+  docker push ${REPOSITORY_PREFIX}/spring-petclinic-api-gateway:${VERSION}
+  docker push ${REPOSITORY_PREFIX}/spring-petclinic-visits-service:${VERSION}
+  docker push ${REPOSITORY_PREFIX}/spring-petclinic-vets-service:${VERSION}
+  docker push ${REPOSITORY_PREFIX}/spring-petclinic-customers-service:${VERSION}
+  docker push ${REPOSITORY_PREFIX}/spring-petclinic-admin-server:${VERSION}
+fi
 
